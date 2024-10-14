78134e6582f0df107e913d9e9ea84f4335e84316
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-10-15 00:28:05 +0300

    installing entity frameworks and creating category model

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+namespace BulkyWeb.Data;
+
+public class ApplicationDbContext
+{
+    
+}
--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9dfbd870573323f56c9be536251eedea8a5eaec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a794eaf21e76dffddcfda66bd604c517486fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
